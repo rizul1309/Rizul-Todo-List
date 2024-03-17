31ffcc8090d0236bbc434e332ba5a05bf0b0ccd6
Rizul Walia <rizul1309@gmail.com>
2024-03-17 23:31:39 +0530

    setting up of headers

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { addTODO,getallTodos,ToggletodoDone ,updateTodo,deletetodo} from '../controller/todo-controller.js';
-
+import cors from 'cors';
 const route =  express.Router();
+route.use(cors())
 route.post('/todos',addTODO)
 route.get('/todos',getallTodos)
 route.get('/todos/:id',ToggletodoDone)
