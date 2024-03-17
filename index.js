8eb1c917fa684493b82a8318ac12b2dbb0221bfa
Rizul Walia <rizul1309@gmail.com>
2024-03-17 16:31:23 +0530

    setting up of headers

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   
     next();
   });
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',route)
