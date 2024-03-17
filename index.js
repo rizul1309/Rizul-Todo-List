b9bb744f83da36454ef35ea7d43d1bdcfe8064e1
Rizul Walia <rizul1309@gmail.com>
2024-03-17 21:28:45 +0530

    setting up of headers

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 import express from 'express';
 import Connection from './database/db.js';
-const cors = require('cors')
+import cors from 'cors';
 import route from './routes/route.js';
 import bodyParser from 'body-parser';
 
 const app = express();
-const corsOpts = {
-    origin: '*',  
-    methods: [
-      'GET',
-      'POST',
-      'PUT',
-      'DELETE',
-    ],  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-};
-app.use(cors(corsOpts));
+app.use(cors());
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',route)
