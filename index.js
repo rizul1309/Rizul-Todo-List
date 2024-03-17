75622e1a8a5a50711a79fc38f941d973b7b6c20d
Rizul Walia <rizul1309@gmail.com>
2024-03-17 16:13:19 +0530

    cors localhost

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import route from './routes/route.js';
 import bodyParser from 'body-parser';
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',route)
